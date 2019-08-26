fd24baa4f67064ac7b25959c9fc208449a392d9e
steven victor <chikodi543@gmail.com>
2019-08-26 15:23:35 +0100

    Remove files not used

--- api/interfaces/users_interface.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-// type UserInterface interface {
-// SaveUser(models.User) (models.User, error)
-// 	FindAllUsers() ([]models.User, error)
-// 	FindUserByID(uint32) (models.User, error)
-// 	UpdateAUser(uint32, models.User) (models.User, error)
-// 	DeleteAUser(uint32) (int64, error)
-// }
