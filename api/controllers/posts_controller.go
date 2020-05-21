4d40a3ffff1ad74b5b1bec6c87521f91a69d4717
rj <rj@office>
2020-05-21 15:23:41 +0800

    Fixed misspelling of location

--- api/controllers/posts_controller.go
+++ api/controllers/posts_controller.go
@@ -49,7 +49,7 @@ func (server *Server) CreatePost(w http.ResponseWriter, r *http.Request) {
 		responses.ERROR(w, http.StatusInternalServerError, formattedError)
 		return
 	}
-	w.Header().Set("Lacation", fmt.Sprintf("%s%s/%d", r.Host, r.URL.Path, postCreated.ID))
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.URL.Path, postCreated.ID))
 	responses.JSON(w, http.StatusCreated, postCreated)
 }
 
